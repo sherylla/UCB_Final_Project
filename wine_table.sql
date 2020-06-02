23761daa512183b910259815dbb98de808061424
Sheryll Agpalo <sagpalo@salesforce.com>
2020-06-02 15:55:54 -0700

    added sql to create tables

---
+++ wine_table.sql
@@ -0,0 +1,29 @@
+#Create Tables
+CREATE TABLE "Red_Wine_Quality" (
+    "fixed_acidity" float   NOT NULL,
+    "volatile_acidity" float   NOT NULL,
+    "citric_acid" float   NOT NULL,
+    "residual_sugar" float   NOT NULL,
+    "chlorides" float   NOT NULL,
+    "free_sulfur_dioxide" float   NOT NULL,
+    "total_sulfur_dioxide" float   NOT NULL,
+    "density" float   NOT NULL,
+    "pH" float   NOT NULL,
+    "sulphates" float   NOT NULL,
+    "alcohol" float   NOT NULL,
+  "quality" int   NOT NULL
+);
+CREATE TABLE "White_Wine_Quality" (
+    "fixed_acidity" float   NOT NULL,
+    "volatile_acidity" float   NOT NULL,
+    "citric_acid" float   NOT NULL,
+    "residual_sugar" float   NOT NULL,
+    "chlorides" float   NOT NULL,
+    "free_sulfur_dioxide" float   NOT NULL,
+    "total_sulfur_dioxide" float   NOT NULL,
+    "density" float   NOT NULL,
+    "pH" float   NOT NULL,
+    "sulphates" float   NOT NULL,
+    "alcohol" float   NOT NULL,
+  "quality" int   NOT NULL
+);
