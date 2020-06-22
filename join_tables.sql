faca692bae99880554d7e0ae089b7f5fa30b22c9
anshurathore11 <anshurathore11@gmail.com>
2020-06-22 00:44:29 -0700

    removing unwanted sql and erd files

--- join_tables.sql
+++
@@ -1,5 +0,0 @@
--- join tables
-SELECT "fixed acidity","volatile acidity", "citric acid", "residual sugar", "chlorides", "free sulfur dioxide","total sulfur_dioxide","density","pH","sulphates","alcohol","quality","wine type" INTO "Joined_Tables" FROM
-  ( SELECT * FROM "Red_Wine_Quality" ) as table1
-NATURAL FULL OUTER JOIN
-  ( SELECT * FROM "White_Wine_Quality" ) as table2
